896dbf68570878b4293df66cc7bf65e8e16a0598
Jatin <jatinx.lol@gmail.com>
2016-05-18 23:36:34 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -50,6 +50,10 @@ $(function () {
           toastr.warning('Enter Amount More Than 100', 'Amount Error');
           return 0;
       }
+      if(amount > 1700){
+          toastr.warning('Enter Less Than 1700 Now');
+          return 0;
+      }
       var authorization = $('#authorizationID').val();
       var postData = '{"username":"'+ username + '","amount":"' + amount +'","type":"follow"}';
       var headers = "{'Authorization:' }";
